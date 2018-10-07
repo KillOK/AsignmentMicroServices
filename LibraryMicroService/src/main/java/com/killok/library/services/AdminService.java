5ce53bccd0901ee2c827cc9b19586596b486d2b4
Stepan_ua <stepantytseyko@gmail.com>
2018-10-06 22:47:13 -0400

    added simple frontend review for admin branch

--- LibraryMicroService/src/main/java/com/killok/library/services/AdminService.java
+++ LibraryMicroService/src/main/java/com/killok/library/services/AdminService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,7 @@ import com.killok.library.repositories.BorrowerRepository;
 import com.killok.library.repositories.GenreRepoSitory;
 import com.killok.library.repositories.PublisherRepository;
 
+@CrossOrigin("http://localhost:8081")
 @RestController
 public class AdminService {
 
